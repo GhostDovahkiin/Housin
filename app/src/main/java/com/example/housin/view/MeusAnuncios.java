b807b71d129fa54f5c50f486df79d22190b7bc30
Pedro Henrique <pedro.sousa@dce.ufpb.br>
2019-09-19 23:43:21 -0300

    Tela meus anuncios, favoritos, feedback, denuncia

---
+++ app/src/main/java/com/example/housin/view/MeusAnuncios.java
@@ -0,0 +1,15 @@
+package com.example.housin.view;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.housin.R;
+
+public class MeusAnuncios extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meus_anuncios);
+
+    }
+}
