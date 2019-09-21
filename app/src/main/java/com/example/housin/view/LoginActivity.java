b8acf07b564b86fdfe25bdb6bd2c15bdeab8a120
Pedro Henrique <pedro.sousa@dce.ufpb.br>
2019-09-21 00:19:30 -0300

    Adição Firebase e autenticação

--- app/src/main/java/com/example/housin/view/LoginActivity.java
+++ app/src/main/java/com/example/housin/view/LoginActivity.java
@@ -6,6 +6,8 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.housin.R;
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class LoginActivity extends AppCompatActivity {
     Button button;
--- app/src/main/java/com/example/housin/view/MainActivity.java
+++ app/src/main/java/com/example/housin/view/MainActivity.java
@@ -5,10 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
 import com.example.housin.R;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mFirebaseAuth; // Login do Firebase
+    private GoogleApiClient mApiClient; // Login do Google
+    EditText textLogin, textSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +40,83 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        textLogin = this.<EditText>findViewById(R.id.textUsername);
+        textSenha = this.<EditText>findViewById(R.id.textSenha);
+        findViewById(R.id.buttonLogin).setOnClickListener(this);
+        /*findViewById(R.id.buttonLogout).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signOut();
+            }
+        } */
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .build();
+
+        mApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+
+
+    }
+
+    private void signIn(){
+        Intent intent = Auth.GoogleSignInApi.getSignInIntent(mApiClient);
+        startActivityForResult(intent, 1);
+    }
+
+    private void signOut(){
+        mFirebaseAuth.signOut();
+        Auth.GoogleSignInApi.signOut(mApiClient).setResultCallback(new ResultCallback<Status>() {
+            @Override
+            public void onResult(@NonNull Status status) {
+                Toast.makeText(MainActivity.this, "Conta desconectada", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1){
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+
+            if (result.isSuccess()){
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseLogin(account);
+            }
+        }
+    }
+
+    public void firebaseLogin(GoogleSignInAccount account){
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+
+        mFirebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            //startActivity(new Intent(MainActivity.this, feed.class));
+                            finish();
+                        }else {
+                            Toast.makeText(MainActivity.this, "Falha na autenticação", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onClick(View v){
+        signIn();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(MainActivity.this, "Falha de autenticação", Toast.LENGTH_LONG).show();
     }
 }
