40a3b2c3dc06a5e9a1066721c46f20a1c10b7abc
Allain-Prost <allain.prost@dce.ufpb.br>
2019-09-22 23:00:13 -0300

    Tela perfil e xml de imagem redond

---
+++ app/src/main/java/com/example/housin/AdicionarMoradiaActivity.java
@@ -0,0 +1,14 @@
+package com.example.housin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdicionarMoradiaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adicionar_moradia);
+    }
+}
