772fa02758a84fdc3587e3a8fa9e624d5d3ae1e9
Allain-Prost <allain.prost@dce.ufpb.br>
2019-09-23 05:23:12 -0300

    trim Login

--- app/src/main/java/com/example/housin/view/MainActivity.java
+++ app/src/main/java/com/example/housin/view/MainActivity.java
@@ -48,8 +48,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        String user = editTextUser.getText().toString();
-        String senha = editTextSenha.getText().toString();
+        String user = editTextUser.getText().toString().trim();
+        String senha = editTextSenha.getText().toString().trim();
 
         if(user.isEmpty()){
             editTextUser.setError("Email é requerido");
